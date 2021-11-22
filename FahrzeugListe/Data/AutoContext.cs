f6d808862398e28f50d8066b060c4e725cabc646
Narukxge <93284922+Narukxge@users.noreply.github.com>
2021-11-22 14:19:47 +0100

    adsa

---
+++ FahrzeugListe/Data/AutoContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FahrzeugListe.Models;
+
+namespace FahrzeugListe.Data
+{
+    public class AutoContext : DbContext
+    {
+        public AutoContext(DbContextOptions<AutoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Auto> Autos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Auto>().ToTable("Auto");
+        }
+    }
+}
---
+++ FahrzeugListe/Data/DbInitiazlier.cs
@@ -0,0 +1,33 @@
+﻿using FahrzeugListe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FahrzeugListe.Data
+{
+    public class DbInitiazlier
+    {
+        public static void Initialize(AutoContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Autos.Any())
+            {
+                return;
+            } // Ist dafür da das wenn man es nochmal startet das es nicht nochmal in die Liste reinkommt 
+
+            var autos = new Auto[]
+            {
+                new Auto{Fahrzeug="Audi",Kennzeichen="BVG2012",Standort="Berlin"},
+                new Auto{Fahrzeug="BMW",Kennzeichen="DS3212",Standort="Berlin"},
+                new Auto{Fahrzeug="Volvo",Kennzeichen="KID420",Standort="Berlin"}
+            };
+            foreach(Auto a in autos)
+            {
+                context.Autos.Add(a);
+            }
+            context.SaveChanges();
+        }
+    }
+}
